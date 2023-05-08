e901561e7155124c903d4fdb288d763360cf80d5
ABHAY GUPTA <Abhay.19201@knit.ac.in>
2023-05-08 15:23:49 +0530

    change

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 const express = require('express')
 const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
-})
-app.listen(process.env.PORT || 3000)
+
+app.all("/*", (req, res) => {
+    res.json({
+      method: req.method,
+      url: req.url,
+      header: req.headers,
+      body: req.body,
+      query: req.query,
+      params: req.params,
+      cookies: req.cookies,
+    });
+  });
+  
+  app.listen(process.env.PORT || 3000)
